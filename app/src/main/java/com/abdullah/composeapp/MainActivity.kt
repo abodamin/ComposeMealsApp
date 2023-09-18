901a1bb635835893e9bd1346c959936768f9d165
Abdullah Alamin <aamin@saudivts.com>
2023-09-18 15:38:57 +0300

    - Added dagger2 DI

--- app/src/main/java/com/abdullah/composeapp/MainActivity.kt
+++ app/src/main/java/com/abdullah/composeapp/MainActivity.kt
@@ -12,13 +12,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.abdullah.composeapp.ui.meals.MealsPage
 import com.abdullah.composeapp.ui.theme.AppThemeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppThemeAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
---
+++ app/src/main/java/com/abdullah/composeapp/data/di/app.kt
@@ -0,0 +1,10 @@
+package com.abdullah.composeapp.data.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application(){
+
+
+}
