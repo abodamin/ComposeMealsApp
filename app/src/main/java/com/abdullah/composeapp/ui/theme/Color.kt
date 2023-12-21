6cef11f5074c30cec334910039c4c59287dd2b65
Abdullah Alamin <aamin@saudivts.com>
2023-12-21 20:29:37 +0300

    - Change UI theme colors to orange

--- app/src/main/java/com/abdullah/composeapp/ui/theme/Color.kt
+++ app/src/main/java/com/abdullah/composeapp/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.abdullah.composeapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val primaryColorLight = Color(0xFFF7C884)
+val primaryColor = Color(0xFFFF9800)
+val secondaryColor = Color(0xFF4CAF50)
 val Teal200 = Color(0xFF03DAC5)
 val Grey = Color(0xFFACACAC)
 val Red = Color(0xFFCE0C0C)
--- app/src/main/java/com/abdullah/composeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/abdullah/composeapp/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
+    primary = primaryColorLight,
+    primaryVariant = secondaryColor,
     secondary = Teal200,
 //    lightGrey = Grey
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
+    primary = primaryColor,
+    primaryVariant = secondaryColor,
     secondary = Teal200,
 //    lightGrey = Grey
 
