1c9a8be69bab4425c54effa25e651dbc9e3bc6f9
Abdullah Alamin <aamin@saudivts.com>
2024-02-23 05:53:51 +0300

    - UI enhancement on FilterChips padding

--- app/src/main/java/com/abdullah/composeapp/ui/meals/MealsPage.kt
+++ app/src/main/java/com/abdullah/composeapp/ui/meals/MealsPage.kt
@@ -233,7 +233,7 @@ fun MealsPage(
 
                 // Other items in the list
                 item {
-                    LazyRow(modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+                    LazyRow(modifier = Modifier.padding(top = 4.dp, bottom = 4.dp, start = 8.dp)) {
                         items(mViewModel.createSampleCategoryList().size) {
                             CategoryFilterChip(
                                 title = mViewModel.createSampleCategoryList()[it].strCategory,
